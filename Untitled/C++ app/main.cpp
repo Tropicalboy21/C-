5d36e97c2ce45b066eb9622f80160d7ff1a3c1b5
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-08 06:04:00 -0600

    switch

--- Untitled/C++ app/main.cpp
+++ Untitled/C++ app/main.cpp
@@ -5,17 +5,18 @@ int main()
 {
     // Write C++ code here
     int x;
-    int y;
     cout << "Enter the a value:";
     cin >> x;
-    cout << "Enter the second value:";
-    cin >> y;
-    if (x > y)
-        cout << "first value is greater than the second value";
-    else if (x < y)
-        cout << "second value is greater than the first value";
-    else
-        cout << "both values are equal";
+
+    switch (expression)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
 
     return 0;
 }
