fa367355a4df0c418795563b7efe8dbd459fdc26
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-08 06:09:21 -0600

    practice

--- Untitled/C++ app/main.cpp
+++ Untitled/C++ app/main.cpp
@@ -13,8 +13,13 @@ int main()
     case 1:
         cout << "x is equal to 1\n";
         break;
+    case 2:
+    case 3:
+        cout << "x is equal to 2 or 3\n";
+        break;
 
     default:
+        cout << "x is not 1, 2, or 3 ";
         break;
     }
 
