0db8a0ec2ecd5a33d809328f8016c19734d32a56
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-11 21:47:04 -0600

    practice

---
+++ Untitled/C++ app/program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 0;
+    cout << "Enter the a value:";
+    cin >> x;
+
+    const string message = "Hello, world!";
+    cout << message;
+    return 0;
+}
