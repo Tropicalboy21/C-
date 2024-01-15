6e8c80108f7987ea67f1b2006a22b38b9c33f001
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-15 05:32:29 -0600

    practice

--- Untitled/C++ app/program4.cpp
+++ Untitled/C++ app/program4.cpp
@@ -11,7 +11,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Please enter the digit number" << i + 1;
+        cout << "Please enter the digit number #" << i + 1 << " ";
         cin >> numbers[i];
     }
 
