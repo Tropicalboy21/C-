7ce26045bc730fd0e4fae29d0ef62d3c63bbbfda
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-16 06:32:45 -0600

    practice

--- Untitled/C++ app/program4.cpp
+++ Untitled/C++ app/program4.cpp
@@ -11,23 +11,44 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
+
         cout << "Please enter the digit number #" << i + 1 << " ";
         cin >> numbers[i];
     }
 
-    cout << n % 2;
+    int minNumber = numbers[0];
+    int maxNumber = numbers[0];
+
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int y = 0; y < size; y++)
+    {
+        if (numbers[y] > maxNumber)
+        {
+            maxNumber = numbers[y];
+        }
+        if (numbers[y] < minNumber)
+        {
+            minNumber = numbers[y];
+        }
+    }
+
+    int mean = numbers[0];
+    int sum = 0;
 
     if (n % 2 == 0)
     {
+        sum = numbers[n / 2] + numbers[n / 2 - 1];
+        mean = sum / 2;
     }
     else
     {
-
-        for (int j = 0; j < n; j++)
-        {
-            cout << numbers[j];
-        }
+        mean = numbers[n / 2];
     }
+
+    cout << "mean: " << mean << "\n";
+    cout << "min: " << minNumber << "\n";
+    cout << "max: " << maxNumber << "\n";
 
     return 0;
 }
