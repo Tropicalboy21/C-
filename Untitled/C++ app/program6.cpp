a4b356075d7bd1b607b07b9260e5afeb9119c6e5
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-17 05:41:42 -0600

    practice

--- Untitled/C++ app/program6.cpp
+++ Untitled/C++ app/program6.cpp
@@ -3,4 +3,20 @@ using namespace std;
 
 int main()
 {
+    int n = 2;
+    while (n == 2)
+    {
+        int x = 0;
+        cout << "Please enter a number: ";
+        cin >> x;
+        int c = x % 5;
+
+        if (c == 0)
+        {
+            cout << "number " << x << " is divisible by 5";
+            n = -1;
+        }
+    }
+
+    return 0;
 }
