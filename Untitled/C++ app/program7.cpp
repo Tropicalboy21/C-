0f64b84ec49dc5f9aa501b983681b964f8aa317c
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-18 06:17:22 -0600

    practice

--- Untitled/C++ app/program7.cpp
+++ Untitled/C++ app/program7.cpp
@@ -3,4 +3,13 @@ using namespace std;
 
 int main()
 {
+    short number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "The factorial of " << number << " is";
+    int accumulator = 1;
+    for (; accumulator < number; number++)
+        cout << accumulator << ".\n";
+    return 0;
 }
