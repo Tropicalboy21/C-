837cb857a52f5f3618396cb01299586353ad92c9
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-19 18:44:06 -0600

    practice

---
+++ Untitled/C++ app/marsExploration.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int marsExploration(string s)
+{
+    int l = s.length();
+
+    int numMessages = l / 3;
+
+    string expMessage = "message";
+
+    for (int i = 0; i < numMessages; i++)
+    {
+        expMessage = s
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
