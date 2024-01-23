c96fb20bd7b54d4bc2522b6000b0df4a8c43dacd
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-22 20:29:35 -0600

    C++

---
+++ Untitled/C++ app/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Lenin";
+    string message = "Hello world"
+
+                     cout
+                     << message << return 0;
+}
