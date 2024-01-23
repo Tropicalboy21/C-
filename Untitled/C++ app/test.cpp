09c3b550e4e6fc4d293d6fa5925415a8067d914b
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-22 21:30:54 -0600

    practice

--- Untitled/C++ app/test.cpp
+++ Untitled/C++ app/test.cpp
@@ -3,13 +3,29 @@ using namespace std;
 
 int main()
 {
-    string name = " Lenin";
-    string message = "Hello world my name is";
+    string marcas[5];
+    string marca = "";
+    int i = 0;
+    int opcion = 0;
 
-    cout << "\nBienvenido a la agencia de carros NO ande a Pie ";
-    cout << "----- Menu -----";
-    cout << "Selecione una opcion";
-    cout << "1. registrar marca";
-    cout << "2. listar marcas";
+    while (i <= 5)
+    {
+        cout << "\nBienvenido a la agencia de carros NO ande a Pie ";
+        cout << "----- Menu -----";
+        cout << "Selecione una opcion";
+        cout << "1. registrar marca";
+        cout << "2. listar marcas";
+        cout << "3. Salir";
+
+        cin >> opcion;
+
+        if (opcion == 1)
+        {
+            cout << "Ingrese una marca: ";
+            cin >> marca;
+            marcas[i] =
+        }
+    }
+
     return 0;
 }
