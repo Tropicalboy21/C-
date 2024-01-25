7c227357c9c3f3ed9656932cf2ab7d8a3e9f3d23
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-24 22:23:50 -0600

    practice

--- Untitled/C++ app/agencia.cpp
+++ Untitled/C++ app/agencia.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 void menu()
 {
-    switch (expression)
-    {
-    case /* constant-expression */:
-        /* code */
-        break;
-
-    default:
-        break;
-    }
+    cout << "Bienvenido a la agencia de carros NO ande a Pie"
+         << "\n";
+    cout << "----- Menu -----"
+         << "\n";
+    cout << "Selecione una opcion"
+         << "\n";
+    cout << "1. registrar marca"
+         << "\n";
+    cout << "2. listar marcas"
+         << "\n";
+    cout << "3. Salir"
+         << "\n";
 }
 
 int main()
