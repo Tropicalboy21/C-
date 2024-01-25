2bd2bb94b75b7a71bbd25d8d71fccb9cb572a3e2
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-24 22:22:43 -0600

    practice

--- Untitled/C++ app/agencia.cpp
+++ Untitled/C++ app/agencia.cpp
@@ -3,4 +3,18 @@ using namespace std;
 
 int main()
 {
+    menu();
+}
+
+void menu()
+{
+    switch (expression)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
 }
