0a5e6789e7265835f3bbf80f0f09b2d836e63226
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-01-24 22:29:50 -0600

    practice

--- Untitled/C++ app/agencia.cpp
+++ Untitled/C++ app/agencia.cpp
@@ -5,19 +5,22 @@ int opcion = 0;
 
 void menu()
 {
-
-    cout << "Bienvenido a la agencia de carros NO ande a Pie"
-         << "\n";
-    cout << "----- Menu -----"
-         << "\n";
-    cout << "1. registrar marca"
-         << "\n";
-    cout << "2. listar marcas"
-         << "\n";
-    cout << "3. Salir"
-         << "\n";
-    cout << "Porfavor selecione una opcion"
-         << "\n";
+    do
+    {
+        cout << "Bienvenido a la agencia de carros NO ande a Pie"
+             << "\n";
+        cout << "----- Menu -----"
+             << "\n";
+        cout << "1. registrar marca"
+             << "\n";
+        cout << "2. listar marcas"
+             << "\n";
+        cout << "3. Salir"
+             << "\n";
+        cout << "Porfavor selecione una opcion"
+             << "\n";
+        cin >> opcion;
+    } while (opcion != 4);
 }
 
 int main()
