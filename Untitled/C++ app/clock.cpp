877af35663a30f80e02dc9b487d0511b2090aadf
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-04 20:37:48 -0600

    practice

--- Untitled/C++ app/clock.cpp
+++ Untitled/C++ app/clock.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 void menu()
 {
+    do
+    {
+
+        cout << "\n"
+             << "Registro de entradas y salidas"
+             << "\n";
+        cout << "----- Menu -----"
+             << "\n";
+        cout << "1. registrar empleado"
+             << "\n";
+        cout << "2. registrar entrads y salidas"
+             << "\n";
+        cout << "3. listar empleados"
+             << "\n";
+        cout << "4. Salir"
+             << "\n";
+        cout << "Porfavor selecione una opcion: ";
+        cin >> opcion;
+        procesarOpcion(opcion);
+    } while (opcion != 5);
 }
 
 int main()
