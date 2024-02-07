0ec9e29209c15c679af3a32a73393466ad67c812
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-06 20:13:55 -0600

    practice

--- Untitled/C++ app/clock.cpp
+++ Untitled/C++ app/clock.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int opcion = 0;
+
 void menu()
 {
-    do
-    {
+     do
+     {
 
-        cout << "\n"
-             << "Registro de entradas y salidas"
-             << "\n";
-        cout << "----- Menu -----"
-             << "\n";
-        cout << "1. registrar empleado"
-             << "\n";
-        cout << "2. registrar entrads y salidas"
-             << "\n";
-        cout << "3. listar empleados"
-             << "\n";
-        cout << "4. Salir"
-             << "\n";
-        cout << "Porfavor selecione una opcion: ";
-        cin >> opcion;
-        procesarOpcion(opcion);
-    } while (opcion != 5);
+          cout << "\n"
+               << "Registro de entradas y salidas"
+               << "\n";
+          cout << "----- Menu -----"
+               << "\n";
+          cout << "1. registrar empleado"
+               << "\n";
+          cout << "2. registrar entrads y salidas"
+               << "\n";
+          cout << "3. listar empleados"
+               << "\n";
+          cout << "4. Salir"
+               << "\n";
+          cout << "Porfavor selecione una opcion: ";
+          cin >> opcion;
+          //    procesarOpcion(opcion);
+     } while (opcion != 5);
 }
 
 int main()
 {
-    menu();
-    return 0
+     menu();
+     return 0;
 }
