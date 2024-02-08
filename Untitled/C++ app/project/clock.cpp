45770fced1910d8ddf02e8483da5f5334674a328
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-08 06:28:50 -0600

    practice

--- Untitled/C++ app/project/clock.cpp
+++ Untitled/C++ app/project/clock.cpp
@@ -14,6 +14,19 @@ void listarEmpleado()
 void registrarEmpleado()
 {
      cout << "digite el nombre del empleado";
+     string nombreIn;
+
+     cout << "Porfavor ingrese la identificación del empleado: ";
+     string identificacionIn = sc.next();
+
+     cout << "Porfavor ingrese un genero F. Femenino, M. Masculino, O. No decir : ";
+     string generoIn;
+
+     cout << "Porfavor ingrese la edad: ";
+     int edadIn;
+
+     cout << "Porfavor ingrese la posición de trabajo: ";
+     string puestoIn;
 }
 
 void procesarOpcion()
