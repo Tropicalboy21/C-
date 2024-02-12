0dace86bd8c4d597f10b05c687849a9af9ee5121
Lenin Ugalde <ugaldelenin@hotmail.com>
2024-02-11 21:38:37 -0600

    C++

--- Untitled/C++ app/project/File1.cpp
+++ Untitled/C++ app/project/File1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 Empleado::Empleado(string nombre, string identificacion, string genero, int edad, string puesto)
 {
-    this.nombre = nombre;
-    this.identificacion = identificacion;
-    this.genero = genero;
-    this.edad = edad;
-    this.puesto = puesto;
+    nombre = nombre;
+    identificacion = identificacion;
+    genero = genero;
+    edad = edad;
+    puesto = puesto;
 };
